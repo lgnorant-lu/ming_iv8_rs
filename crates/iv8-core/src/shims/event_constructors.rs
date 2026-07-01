4b818028b3d1cc24a2eff70eb1fbc787c8215b16
lgnorant-lu <2953105263@qq.com>
2026-07-02 05:36:07 +0800

    fix(shims): Event prototype accessor properties
    实施内容:
    - event_constructors.rs: Event/CustomEvent/MouseEvent/KeyboardEvent/
      PointerEvent 属性从实例 data property 改为 prototype accessor property
      + _defAccessor: 可读写 accessor (type/returnValue/cancelBubble)
      + _defReadOnly: 只读 accessor (bubbles/cancelable/composed/target/
        currentTarget/srcElement/eventPhase/timeStamp/isTrusted/
        defaultPrevented)
      + 构造器中设置 _slot 实例属性, prototype accessor 从 slot 读取
      + MouseEvent: clientX/clientY/screenX/screenY/pageX/pageY/offsetX/
        offsetY/x/y/button/buttons/ctrlKey/shiftKey/altKey/metaKey/
        relatedTarget 全部改为 prototype accessor
      + KeyboardEvent: key/code/keyCode/charCode/which/ctrlKey/shiftKey/
        altKey/metaKey/repeat/location 同上
      + PointerEvent: pointerId/width/height/pressure/pointerType/isPrimary
      修复 idlharness "The prototype object must have a property" (48 FAILs)
    - 验证: 主 isolate 3306/3758 (87.97%), +6 PASS
      Worker variant 396/436 无 regression
    
    本提交不授权:
    - Document prototype 缺属性 (domain/referrer/cookie 等, 需在 document_props.rs 添加)
    - MessagePort prototype 缺属性 (onclose/onmessage/onmessageerror)
    
    已执行审阅:
    - uv run maturin develop --profile dev: PASS
    - run_wpt.py: 3306/3758 (87.97%)
    - run_wpt_worker.py: 396/436 (90.8%)

--- crates/iv8-core/src/shims/event_constructors.rs
+++ crates/iv8-core/src/shims/event_constructors.rs
@@ -2,32 +2,72 @@
 //!
 //! Installed as global classes via JS shim.
 
-/// JS shim that installs Event, CustomEvent, MouseEvent constructors.
 pub const EVENT_CONSTRUCTORS_JS: &str = r#"
 (function() {
-    // ─── Event ──────────────────────────────────────────────────────────────
+    function _defAccessor(proto, name, defaultVal) {
+        var slot = '_' + name;
+        Object.defineProperty(proto, name, {
+            get: function() {
+                var v = this[slot];
+                return v !== undefined ? v : defaultVal;
+            },
+            set: function(v) { this[slot] = v; },
+            enumerable: true,
+            configurable: true
+        });
+    }
+    function _defReadOnly(proto, name, defaultVal) {
+        var slot = '_' + name;
+        Object.defineProperty(proto, name, {
+            get: function() {
+                var v = this[slot];
+                return v !== undefined ? v : defaultVal;
+            },
+            set: undefined,
+            enumerable: true,
+            configurable: true
+        });
+    }
+
     function Event(type, options) {
         if (!(this instanceof Event)) {
             throw new TypeError("Failed to construct 'Event': Please use the 'new' operator");
         }
         options = options || {};
-        this.type = type || '';
-        this.bubbles = !!options.bubbles;
-        this.cancelable = options.cancelable !== undefined ? !!options.cancelable : false;
-        this.composed = !!options.composed;
-        this.defaultPrevented = false;
-        this.target = null;
-        this.currentTarget = null;
-        this.eventPhase = 0;
-        this.timeStamp = Date.now();
-        this.isTrusted = false;
+        this._type = type || '';
+        this._bubbles = !!options.bubbles;
+        this._cancelable = options.cancelable !== undefined ? !!options.cancelable : false;
+        this._composed = !!options.composed;
+        this._defaultPrevented = false;
+        this._target = null;
+        this._currentTarget = null;
+        this._srcElement = null;
+        this._eventPhase = 0;
+        this._timeStamp = Date.now();
+        this._isTrusted = false;
+        this._returnValue = true;
+        this._cancelBubble = false;
         this._stopPropagation = false;
         this._stopImmediatePropagation = false;
     }
 
+    _defAccessor(Event.prototype, 'type', '');
+    _defReadOnly(Event.prototype, 'bubbles', false);
+    _defReadOnly(Event.prototype, 'cancelable', false);
+    _defReadOnly(Event.prototype, 'composed', false);
+    _defReadOnly(Event.prototype, 'defaultPrevented', false);
+    _defReadOnly(Event.prototype, 'target', null);
+    _defReadOnly(Event.prototype, 'srcElement', null);
+    _defReadOnly(Event.prototype, 'currentTarget', null);
+    _defReadOnly(Event.prototype, 'eventPhase', 0);
+    _defReadOnly(Event.prototype, 'timeStamp', 0);
+    _defReadOnly(Event.prototype, 'isTrusted', false);
+    _defAccessor(Event.prototype, 'returnValue', true);
+    _defAccessor(Event.prototype, 'cancelBubble', false);
+
     Event.prototype.preventDefault = function preventDefault() {
-        if (this.cancelable) {
-            this.defaultPrevented = true;
+        if (this._cancelable) {
+            this._defaultPrevented = true;
         }
     };
 
@@ -49,7 +89,6 @@ pub const EVENT_CONSTRUCTORS_JS: &str = r#"
     Event.AT_TARGET = 2;
     Event.BUBBLING_PHASE = 3;
 
-    // Make prototype non-writable, non-configurable, non-enumerable
     Object.defineProperty(Event, 'prototype', {writable: false, enumerable: false, configurable: false});
     Object.defineProperty(Event, 'length', {value: 1, writable: false, enumerable: false, configurable: true});
     ['NONE', 'CAPTURING_PHASE', 'AT_TARGET', 'BUBBLING_PHASE'].forEach(function(k) {
@@ -58,80 +97,100 @@ pub const EVENT_CONSTRUCTORS_JS: &str = r#"
 
     globalThis.Event = Event;
 
-    // ─── CustomEvent ────────────────────────────────────────────────────────
     function CustomEvent(type, options) {
         Event.call(this, type, options);
         options = options || {};
-        this.detail = options.detail !== undefined ? options.detail : null;
+        this._detail = options.detail !== undefined ? options.detail : null;
     }
     CustomEvent.prototype = Object.create(Event.prototype);
     Object.defineProperty(CustomEvent.prototype, 'constructor', {value: CustomEvent, writable: true, enumerable: false, configurable: true});
+    _defReadOnly(CustomEvent.prototype, 'detail', null);
     Object.defineProperty(CustomEvent, 'prototype', {writable: false, enumerable: false, configurable: false});
     Object.defineProperty(CustomEvent, 'length', {value: 1, writable: false, enumerable: false, configurable: true});
 
     globalThis.CustomEvent = CustomEvent;
 
-    // ─── MouseEvent ─────────────────────────────────────────────────────────
     function MouseEvent(type, options) {
         Event.call(this, type, options);
         options = options || {};
-        this.clientX = options.clientX || 0;
-        this.clientY = options.clientY || 0;
-        this.screenX = options.screenX || 0;
-        this.screenY = options.screenY || 0;
-        this.pageX = options.pageX || 0;
-        this.pageY = options.pageY || 0;
-        this.offsetX = options.offsetX || 0;
-        this.offsetY = options.offsetY || 0;
-        this.button = options.button || 0;
-        this.buttons = options.buttons || 0;
-        this.ctrlKey = !!options.ctrlKey;
-        this.shiftKey = !!options.shiftKey;
-        this.altKey = !!options.altKey;
-        this.metaKey = !!options.metaKey;
-        this.relatedTarget = options.relatedTarget || null;
+        this._clientX = options.clientX || 0;
+        this._clientY = options.clientY || 0;
+        this._screenX = options.screenX || 0;
+        this._screenY = options.screenY || 0;
+        this._pageX = options.pageX || 0;
+        this._pageY = options.pageY || 0;
+        this._offsetX = options.offsetX || 0;
+        this._offsetY = options.offsetY || 0;
+        this._x = options.clientX || 0;
+        this._y = options.clientY || 0;
+        this._button = options.button || 0;
+        this._buttons = options.buttons || 0;
+        this._ctrlKey = !!options.ctrlKey;
+        this._shiftKey = !!options.shiftKey;
+        this._altKey = !!options.altKey;
+        this._metaKey = !!options.metaKey;
+        this._relatedTarget = options.relatedTarget || null;
     }
     MouseEvent.prototype = Object.create(Event.prototype);
     Object.defineProperty(MouseEvent.prototype, 'constructor', {value: MouseEvent, writable: true, enumerable: false, configurable: true});
+    ['clientX','clientY','screenX','screenY','pageX','pageY','offsetX','offsetY','x','y','button','buttons'].forEach(function(prop) {
+        _defReadOnly(MouseEvent.prototype, prop, 0);
+    });
+    ['ctrlKey','shiftKey','altKey','metaKey'].forEach(function(prop) {
+        _defReadOnly(MouseEvent.prototype, prop, false);
+    });
+    _defReadOnly(MouseEvent.prototype, 'relatedTarget', null);
     Object.defineProperty(MouseEvent, 'prototype', {writable: false, enumerable: false, configurable: false});
 
     globalThis.MouseEvent = MouseEvent;
 
-    // ─── KeyboardEvent ──────────────────────────────────────────────────────
     function KeyboardEvent(type, options) {
         Event.call(this, type, options);
         options = options || {};
-        this.key = options.key || '';
-        this.code = options.code || '';
-        this.keyCode = options.keyCode || 0;
-        this.charCode = options.charCode || 0;
-        this.which = options.which || options.keyCode || 0;
-        this.ctrlKey = !!options.ctrlKey;
-        this.shiftKey = !!options.shiftKey;
-        this.altKey = !!options.altKey;
-        this.metaKey = !!options.metaKey;
-        this.repeat = !!options.repeat;
-        this.location = options.location || 0;
+        this._key = options.key || '';
+        this._code = options.code || '';
+        this._keyCode = options.keyCode || 0;
+        this._charCode = options.charCode || 0;
+        this._which = options.which || options.keyCode || 0;
+        this._ctrlKey = !!options.ctrlKey;
+        this._shiftKey = !!options.shiftKey;
+        this._altKey = !!options.altKey;
+        this._metaKey = !!options.metaKey;
+        this._repeat = !!options.repeat;
+        this._location = options.location || 0;
     }
     KeyboardEvent.prototype = Object.create(Event.prototype);
     Object.defineProperty(KeyboardEvent.prototype, 'constructor', {value: KeyboardEvent, writable: true, enumerable: false, configurable: true});
+    ['key','code'].forEach(function(prop) {
+        _defReadOnly(KeyboardEvent.prototype, prop, '');
+    });
+    ['keyCode','charCode','which','location'].forEach(function(prop) {
+        _defReadOnly(KeyboardEvent.prototype, prop, 0);
+    });
+    ['ctrlKey','shiftKey','altKey','metaKey','repeat'].forEach(function(prop) {
+        _defReadOnly(KeyboardEvent.prototype, prop, false);
+    });
     Object.defineProperty(KeyboardEvent, 'prototype', {writable: false, enumerable: false, configurable: false});
 
     globalThis.KeyboardEvent = KeyboardEvent;
 
-    // ─── PointerEvent ───────────────────────────────────────────────────────
     function PointerEvent(type, options) {
         MouseEvent.call(this, type, options);
         options = options || {};
-        this.pointerId = options.pointerId || 0;
-        this.width = options.width || 1;
-        this.height = options.height || 1;
-        this.pressure = options.pressure || 0;
-        this.pointerType = options.pointerType || 'mouse';
-        this.isPrimary = options.isPrimary !== undefined ? !!options.isPrimary : true;
+        this._pointerId = options.pointerId || 0;
+        this._width = options.width || 1;
+        this._height = options.height || 1;
+        this._pressure = options.pressure || 0;
+        this._pointerType = options.pointerType || 'mouse';
+        this._isPrimary = options.isPrimary !== undefined ? !!options.isPrimary : true;
     }
     PointerEvent.prototype = Object.create(MouseEvent.prototype);
     Object.defineProperty(PointerEvent.prototype, 'constructor', {value: PointerEvent, writable: true, enumerable: false, configurable: true});
+    ['pointerId','width','height','pressure'].forEach(function(prop) {
+        _defReadOnly(PointerEvent.prototype, prop, 0);
+    });
+    _defReadOnly(PointerEvent.prototype, 'pointerType', 'mouse');
+    _defReadOnly(PointerEvent.prototype, 'isPrimary', true);
     Object.defineProperty(PointerEvent, 'prototype', {writable: false, enumerable: false, configurable: false});
 
     globalThis.PointerEvent = PointerEvent;
